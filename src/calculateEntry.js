cf06d2c0bda1e798791d6d8c211def1219ecc761
Nícolas <nicolasbrga01@gmail.com>
2022-10-31 18:28:40 -0300

    Requisito 8 feito (ordem doida mas o importante é fazer)

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
 const data = require('../data/zoo_data');
 
-function countEntrants(entrants) {
-  // seu código aqui
-}
+const { prices } = data;
 
-function calculateEntry(entrants) {
-  // seu código aqui
-}
+const countEntrants = (entrants) => {
+  const result = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach(({ age }) => {
+    if (age < 18) {
+      result.child += 1;
+    } else if (age >= 18 && age < 50) {
+      result.adult += 1;
+    } else {
+      result.senior += 1;
+    }
+  });
+  return result;
+};
+
+const calculateEntry = (entrants) => {
+  if (entrants === undefined || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const count = countEntrants(entrants);
+  const entries = Object.entries(count);
+  return entries.reduce((total, [nome, much]) => total + much * prices[nome], 0);
+};
 
 module.exports = { calculateEntry, countEntrants };
