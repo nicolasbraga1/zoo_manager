fe403451713d54bb704647006be873693656a4fc
Nícolas <nicolasbrga01@gmail.com>
2022-11-01 13:46:31 -0300

    Requisito 4 feito (quase lá)

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,13 +1,22 @@
 const data = require('../data/zoo_data');
 
-// const {employees} = data;
+const { employees } = data;
 
-const isManager = (id) => {
-  // if (id === stephanieId || id === )
+const isManager = (employeeId) => {
+  const employ = employees.find((employee) => employee.id === employeeId);
+  if (employ.managers.length === 0 || employ
+    .managers[0] === '9e7d4524-363c-416a-8759-8aa7e50c0992') {
+    return true;
+  }
+  return false;
 };
 
-function getRelatedEmployees(managerId) {
-  // seu código aqui
-}
-
+const getRelatedEmployees = (managerId) => {
+  const overlord = employees.filter((name) => name.managers.includes(managerId));
+  const print = overlord.map((nome) => `${nome.firstName} ${nome.lastName}`);
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return print;
+};
 module.exports = { isManager, getRelatedEmployees };
