12b90fbe168c5ae0836dec7a6c0efd74a25dd49e
Nícolas <nicolasbrga01@gmail.com>
2022-11-01 13:13:55 -0300

    Requisito 9 feito (já não aguentava mais)

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,48 @@
 const data = require('../data/zoo_data');
 
+const days = Object.keys(data.hours);
+
+function office(play) {
+  const { open, close } = data.hours[play];
+  const fechado = (open === 0 && close === 0);
+  return fechado ? 'CLOSED' : `Open from ${open}am until ${close}pm`;
+}
+
+const exhibi = (play) => {
+  const places = data.species.reduce((attractions, { name, availability }) => (
+    availability.includes(play) ? [...attractions, name] : attractions
+  ), []);
+  return places.length === 0 ? 'The zoo will be closed!' : places;
+};
+
+const schedules = (play) => ({
+  [play]: {
+    exhibition: exhibi(play),
+    officeHour: office(play),
+  },
+});
+
+const weekCronogram = () => (
+  days.reduce((schedule, play) => (
+    {
+      ...schedule,
+      ...schedules(play),
+    }
+  ), {})
+);
+
+const animas = (animalName) =>
+  data.species.find(({ name }) => name === animalName).availability;
+
 function getSchedule(scheduleTarget) {
-  // seu código aqui
+  const animals = data.species.map((specie) => specie.name);
+  if (![...days, ...animals].includes(scheduleTarget)) {
+    return weekCronogram();
+  }
+  if (days.includes(scheduleTarget)) {
+    return schedules(scheduleTarget);
+  }
+  return animas(scheduleTarget);
 }
 
 module.exports = getSchedule;
