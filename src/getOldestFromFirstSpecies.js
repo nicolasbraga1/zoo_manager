5dd60f87067d0fa6d0e08648cebdcaf39e4b9137
Nícolas <nicolasbrga01@gmail.com>
2022-11-01 17:02:03 -0300

    Requisito 10 concluído (fiz 80%, agora posso comemorar o meu aniversário)

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,18 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const isResponsibleFor = (funcionario) =>
+  data.employees.find((nome) => nome.id === funcionario).responsibleFor;
+
+const getAnimal = (animalSpecies) =>
+  data.species.find((raça) => raça.id === animalSpecies);
+
+const isOldest = (tipo) => tipo.residents
+  .reduce((subject1, subject2) => (subject1.age > subject2.age ? subject1 : subject2), {});
+
+const getOldestFromFirstSpecies = (id) => {
+  const assigned = getAnimal(isResponsibleFor(id)[0]);
+  const { age, name, sex } = isOldest(assigned);
+  return [name, sex, age];
+};
 
 module.exports = getOldestFromFirstSpecies;
