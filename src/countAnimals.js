8ab80c5e1f2bc56bcd3083ebc7ca14f258df9da2
Nícolas <nicolasbrga01@gmail.com>
2022-11-01 14:52:45 -0300

    Requisito 5 feito (foi mais chato que eu esperava)

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aqui
-}
+const { species } = data;
+
+const everyANimal = () => (
+  species.reduce((counter, { name, residents }) => ({
+    ...counter,
+    [name]: residents.length,
+  }), {})
+);
+
+const countAnimals = (animal) => {
+  if (!animal) return everyANimal();
+  const raça = animal.specie;
+  const genero = animal.sex;
+  const arr = species.find((param) => param.name === raça);
+  const filt = arr.residents.filter((param) => param.sex === genero);
+  if (genero === undefined) {
+    return arr.residents.length;
+  }
+  return filt.length;
+};
 
 module.exports = countAnimals;
